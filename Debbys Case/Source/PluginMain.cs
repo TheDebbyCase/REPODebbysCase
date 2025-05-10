f3ccfbe28943d851d86e24fcb65326b757f2a248
TheDebbyCase <thedebbycase@gmail.com>
2025-05-10 06:14:12 +0100

    Add project files.

---
+++ Debbys Case/Source/PluginMain.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Reflection;
+using System.IO;
+using REPODebbysCase.Config;
+using HarmonyLib;
+using REPOLib.Modules;
+namespace REPODebbysCase
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInDependency(REPOLib.MyPluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
+    public class DebbysCase : BaseUnityPlugin
+    {
+        internal const string modGUID = "deB.DebbysCase";
+        internal const string modName = "Debbys Case";
+        internal const string modVersion = "0.0.1";
+        readonly Harmony harmony = new Harmony(modGUID);
+        internal ManualLogSource log = null!;
+        public static DebbysCase instance;
+        internal DebbysCaseConfig ModConfig { get; private set; } = null!;
+        public void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            log = Logger;
+            Type[] assemblyTypes = Assembly.GetExecutingAssembly().GetTypes();
+            for (int i = 0; i < assemblyTypes.Length; i++)
+            {
+                MethodInfo[] typeMethods = assemblyTypes[i].GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+                for (int j = 0; j < typeMethods.Length; j++)
+                {
+                    object[] methodAttributes;
+                    try
+                    {
+                        methodAttributes = typeMethods[j].GetCustomAttributes(typeof(RuntimeInitializeOnLoadMethodAttribute), false);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                    if (methodAttributes.Length > 0)
+                    {
+                        typeMethods[j].Invoke(null, null);
+                    }
+                }
+            }
+            PropogateLists();
+            HandleContent();
+            DoPatches();
+            log.LogInfo($"{modName} Successfully Loaded");
+        }
+        public void PropogateLists()
+        {
+            AssetBundle bundle = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "wildcardmod"));
+            string[] allAssetPaths = bundle.GetAllAssetNames();
+            for (int i = 0; i < allAssetPaths.Length; i++)
+            {
+                string assetPath = allAssetPaths[i][..allAssetPaths[i].LastIndexOf("/")];
+                switch (assetPath)
+                {
+                    case "assets/Debbys Case/resources/valuables":
+                        {
+                            break;
+                        }
+                    case "assets/Debbys Case/resources/items":
+                        {
+                            break;
+                        }
+                    case "assets/Debbys Case/resources/enemies":
+                        {
+                            break;
+                        }
+                    default:
+                        {
+                            log.LogWarning($"\"{assetPath}\" is not a known asset path, skipping.");
+                            break;
+                        }
+                }
+            }
+        }
+        public void HandleContent()
+        {
+            ModConfig = new DebbysCaseConfig(base.Config);
+            //HandleItems();
+        }
+        //public void HandleItems()
+        //{
+        //    for (int i = 0; i < itemList.Count; i++)
+        //    {
+        //        if (i >= ModConfig.isItemEnabled.Count || ModConfig.isItemEnabled[i].Value)
+        //        {
+        //            Items.RegisterItem(itemList[i]);
+        //            log.LogDebug($"{itemList[i].name} item was loaded!");
+        //        }
+        //        else
+        //        {
+        //            log.LogInfo($"{itemList[i].name} item was disabled!");
+        //        }
+        //    }
+        //}
+        public void DoPatches()
+        {
+            log.LogDebug("Patching Game");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# WILDCARD REPO
+
+Repository for Debby's Case, a mod that aims to add various things with the goal to fit the original vibe of the game!
+
+You can use the [Issues](https://github.com/TheDebbyCase/REPODebbysCase/issues) tab to submit any problems with the mod, or any suggestions you may have.
+Suggestions can also be made directly to me if you can find me on Discord, or on [Bluesky](https://bsky.app/profile/thedebbycase.bsky.social)!
+
+If you wish you can also donate to me over on [Ko-Fi](https://ko-fi.com/thedebbycase)!
+
+See the Thunderstore page [here](https://thunderstore.io/c/repo/p/deB/Debbys_Case),
+or the GitHub page [here](https://github.com/TheDebbyCase/REPODebbysCase)!
+
+## Content so far!
+
+- Valuables  
+
+- Shop Items  
+
+I hope to add more soon! Make suggestions on BlueSky!
+
+## Extra Credits
+
+- [kael](https://bsky.app/profile/kael3.bsky.social) for just being so supportive, I just really wanted to put xem on here!
+- [SunflowerSmith](https://www.twitch.tv/sunflowersmith) for adding my Lethal Company mod to their group's modpack, playing with it on-stream, and making suggestions directly!
+- [WhiteSpike](https://thunderstore.io/c/lethal-company/p/WhiteSpike), [Robyn](https://thunderstore.io/c/lethal-company/p/Mom_Llama), [MattyMatty](https://thunderstore.io/c/lethal-company/p/mattymatty/), [XuXiaolan](https://thunderstore.io/c/lethal-company/p/XuXiaolan), [Zaggy](https://thunderstore.io/c/lethal-company/p/Zaggy1024), [IAmBatby](https://thunderstore.io/c/lethal-company/p/IAmBatby/) for giving me various tips and advice!
+- [qwbarch](https://thunderstore.io/c/lethal-company/p/qwbarch) for being the reason I got into modding in the first place, courtesy of [Mirage](https://thunderstore.io/c/lethal-company/p/qwbarch/Mirage)!
+- [Lunxara](https://www.twitch.tv/lunxara) for keeping my interest alive and letting me play with her!
+- Everyone in the [Modding Discord](https://discord.gg/lcmod) for being and making such a great community, and letting me be a part of it!
